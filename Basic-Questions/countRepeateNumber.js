ee38b3c65a4cd9b55d4dd32d492095e23e11fb4a
sachingour <gour.sachin.sg@gmail.com>
2025-05-23 23:17:24 +0530

    count repeated No.

--- Basic-Questions/countRepeateNumber.js
+++ Basic-Questions/countRepeateNumber.js
@@ -16,4 +16,23 @@ function countNumber(arr, num) {
   return repatNum;
 }
 
+function countRepeatNumber(arr) {
+  const visited = {};
+  for (let i = 0; i < arr.length; i++) {
+    //this conditon help us repeated No.
+    if (visited[arr[i]]) continue;
+    let count = 0;
+    let val = null;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        count++;
+      }
+    }
+    visited[arr[i]] = true;
+    console.log(arr[i], " frequency", count);
+  }
+  console.log(visited);
+  
+}
 console.log(countNumber(arr, number));
+console.log(countRepeatNumber(arr));
