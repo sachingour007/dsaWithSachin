1072eebba75c6c967053877707e49ec8bad627fb
sachingour <gour.sachin.sg@gmail.com>
2025-05-27 22:25:04 +0530

    prime number loop

--- Basic-Questions/isPrimeNum.js
+++ Basic-Questions/isPrimeNum.js
@@ -1,22 +1,45 @@
 //Basic Approches
 
-let num = 6;
+let num = 37;
 
 function isPrime(num) {
   if (isNaN(num)) {
     return "Please Enter Number";
   }
-  if (num % 2 !== 0) {
-    return "Number is Prime";
+  let flag = false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      flag = true;
+      break;
+    }
+  }
+
+  if (!flag) {
+    console.log("Num is Prime");
   } else {
-    return "Not Prime";
+    console.log("Num is Not Prime");
   }
 }
 
-
 //Advance Approches
 
+function printPrime(num) {
+  const primeArr = [];
 
-
+  for (let i = 2; i <= num; i++) {
+    let flag = true;
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      primeArr.push(i);
+    }
+  }
+  return primeArr;
+}
 
 console.log(isPrime(num));
+console.log(printPrime(num));
