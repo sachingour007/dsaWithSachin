a25f2e84185ae6e8dd3e3e58570e076f6497c7de
sachingour <gour.sachin.sg@gmail.com>
2025-06-06 09:59:30 +0530

    some loops questions

---
+++ Warm-up Question/loop-02.js
@@ -0,0 +1,47 @@
+//find the given element and return the index.
+
+let arr = [4, 2, 0, 10, 8, 30];
+let num = 10;
+
+function findElement(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}
+const ans = findElement(arr, num);
+console.log(ans);
+
+//Find the Nagative numbes counts
+
+let arrNegative = [2, -9, 17, 0, 1, -10, -4, -8];
+
+function countNegatives(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countNegatives(arrNegative));
+
+//Write a Function return largest Number in array...
+
+let arr0 = [5, 0, 10, , 8, 17, 20];
+
+function findLargestNumber(arr) {
+  let largeNum = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largeNum) {
+      largeNum = arr[i];
+    }
+  }
+  return largeNum;
+}
+
+console.log(findLargestNumber(arr0));
